f293c801631072855489dc5045d935b90280e0a4
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-04-16 14:44:12 +0530

    commited

--- queueusinglinkedlist.c
+++ queueusinglinkedlist.c
@@ -4,7 +4,7 @@
 
 struct node
 {
-    in data;  //4 byt
+    int data;  //4 byt
     //self refferential structure
     struct node *next; //it will have node pointer
 };//one statement
@@ -14,12 +14,12 @@ struct node
 //
 struct node *front=NULL,*rear=NULL; //front is a pointer pointing node of structure
 
-void enqueue(int value)
+void enqueue(int value) //enque = o(1) append = o(n)
 {
     struct node *newnode=NULL;
   //malloc() large byte of memory by specified size and it return starting address
   //malloc() will return genaric or void data type of starting address 
-  newnode = (struct node *)malloc(sizeof(struct node));
+  newnode = (struct node *)malloc(sizeof(struct node)); //8 bytes allocated
   newnode->data=value;
   newnode->next=NULL;
   
@@ -35,10 +35,47 @@ void enqueue(int value)
   
 }
 
+void dequeue()
+{
+    if(front==NULL && rear==NULL)
+    {
+        printf("Queue is empty");
+    }
+    else if(front==rear)
+    {
+        front=rear=NULL;
+    }
+    else
+    {
+        struct node * temp=front;
+        front=front->next;
+        temp->next=NULL;
+        free(temp);
+    }
+}
 
+void disp()
+{
+    struct node * temp=front;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
 
 int main() {
     
+    enqueue(99);
     enqueue(99); 
+    disp();
+    printf("\n");
+    dequeue();
+    dequeue();
+    enqueue(5);
+    disp();
+    printf("\n");
+    
+    //dequeue();
     return 0;
 }
